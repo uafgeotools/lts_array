fa68825ebeaf8ed4850ec1f6b3b0dd5f737e9754
David Fee <dfee1@alaska.edu>
2019-12-08 07:00:40 -0900

    remove scipy and numpy from setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
    url="https://github.com/uafgeotools/lts_array",
    packages=find_packages(),
    python_requires='>=3.0',
-   install_requires=['numpy', 'scipy', 'obspy'],
+   install_requires=['obspy'],
    scripts=[
             'Example_Processing.py',
            ]
