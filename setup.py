f2a98eda9a35efac2299c2ee289857328552ba81
Jordan W Bishop <jwbishop2@alaska.edu>
2020-02-14 14:11:54 -0900

    Update conf.py and setup.py for RTD

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from setuptools import setup, find_packages
+import os as os
+
+# https://github.com/readthedocs/readthedocs.org/issues/5512#issuecomment-475073310
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    INSTALL_REQUIRES = []
+else:
+    INSTALL_REQUIRES = ['matplotlib', 'numpy', 'obspy', 'scipy']
 
 setup(
    name='lts_array',
@@ -9,7 +17,7 @@ setup(
    url="https://github.com/uafgeotools/lts_array",
    packages=find_packages(),
    python_requires='>=3.0',
-   install_requires=['obspy'],
+   install_requires=INSTALL_REQUIRES,
    scripts=[
             'Example_Processing.py',
            ]
